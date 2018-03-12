409ebd164e1a572c3fc6b46b631c587711e3c579
cko053 <cko053>
2018-03-12 08:48:36 +0100

    Switching Row2 and Row3 with Row1 now works

--- Mat121/Mat121/Form1.Designer.cs
+++ Mat121/Mat121/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.programBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -74,7 +75,7 @@
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(15, 78);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(142, 113);
+            this.richTextBox1.Size = new System.Drawing.Size(142, 53);
             this.richTextBox1.TabIndex = 6;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -105,6 +106,14 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(190, 78);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(145, 53);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
+            // 
             // programBindingSource
             // 
             this.programBindingSource.DataSource = typeof(Mat121.Program);
@@ -122,6 +131,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
@@ -148,6 +158,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
--- Mat121/Mat121/Form1.cs
+++ Mat121/Mat121/Form1.cs
@@ -58,10 +58,57 @@ namespace Mat121
                 System.Console.WriteLine(all[i]);
                 System.Console.WriteLine(allN[i]);
             }
+            rowR(allN);
+
+        }
+
+        public void rowR(double[] allN)
+        {
+            //col1 = 0,3,6
+            double[] matrix = allN;
             
+            if (matrix.Length == 9)
+            {
 
 
+                if (matrix[0] == 0)
+                {
 
+                }
+                if (matrix[3] == 0)
+                {
+                    double mem = matrix[0];
+                    double mem2 = matrix[1];
+                    double mem3 = matrix[2];
+                    matrix[0] = matrix[3];
+                    matrix[1] = matrix[4];
+                    matrix[2] = matrix[5];
+                    matrix[3] = mem;
+                    matrix[4] = mem2;
+                    matrix[5] = mem3;
+                    //for (int i = 0; i < matrix.Length; i++)
+                    //{
+                    //    richTextBox2.Text = matrix[0] + " " + matrix[1] + " " + matrix[2] + "\n" + matrix[3] + " " + matrix[4] + " " + matrix[5] + "\n" + matrix[6] + " " + matrix[7] + " " + matrix[8];
+                    //}
+                }
+                if (matrix[6] == 0)
+                {
+                    double mem = matrix[0];
+                    double mem2 = matrix[1];
+                    double mem3 = matrix[2];
+                    matrix[0] = matrix[6];
+                    matrix[1] = matrix[7];
+                    matrix[2] = matrix[8];
+                    matrix[6] = mem;
+                    matrix[7] = mem2;
+                    matrix[8] = mem3;
+                }
+            }
+
+            if(matrix.Length == 16)
+            {
+              
+            }
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
